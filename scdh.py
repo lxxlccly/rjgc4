a88b4b0876fb3563178ce40d47ae07b81da62426
lxxlccly <975716738@qq.com>
2020-03-18 08:45:42 +0800

    生成题目，打印正确答案

--- scdh.py
+++ scdh.py
@@ -1,9 +1,11 @@
+'''诗词大赛'''
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
 import re
 import json
 import eventlet
+import random
 
 
 class PoetGame(object):
@@ -11,31 +13,54 @@ class PoetGame(object):
         self.question_amount = 10
         self.right_answer = []
         self.grade = 0
+        self.questions = []
 
     def start(self):
+        '''开始游戏'''
         for i in range(self.question_amount):
             self.get_question(i)
             self.answer_question()
+            print('正确答案为：' + self.right_answer[i])
 
     def answer_question(self):
+        '''回答问题'''
         eventlet.monkey_patch()
         with eventlet.Timeout(3, False):
             print('hhh')
 
+    def get_sentence(self):
+        '''获得一个诗句'''
+        random_poet = random.randint(0, 57999)
+        address = './poet/poet.song.' + str(int(random_poet // 1000 * 1000)) + '.json'
+        with open(address, 'r', encoding='utf-8') as load_f:
+            load_dict = json.load(load_f)
+            sentence_amount = len(load_dict[int(random_poet % 1000)]['paragraphs'])
+            random_sentence = random.randint(0, sentence_amount - 1)
+            sentences = re.findall(r'[\u4E00-\u9FA5]+',
+                                  load_dict[int(random_poet % 1000)]['paragraphs'][random_sentence])
+            random_sentence = random.randint(0, len(sentences) - 1)
+            sentence = sentences[random_sentence]
+        self.right_answer.append(sentence)
+        words = re.findall(r'[\u4E00-\u9FA5]', sentence)
+        self.questions.append(words)
+
+    def get_disturb(self, number, len_sentence):
+        '''获得对诗句进行干扰的汉字'''
+        len_disturb = 12 - len_sentence
+        for i in range(len_disturb):
+            word = random.randint(0x4e00, 0x9fbf)
+            self.questions[number].append(chr(word))
+
     def get_question(self, number):
+        '''获得题目'''
         print('第%u题：' % int(number + 1))
-        for i in range(58):
-            address = './poet/poet.song.' + str(i * 1000) + '.json'
-            with open(address, 'r', encoding='utf-8') as load_f:
-                load_dict = json.load(load_f)
-                print(load_dict[0]['author'])
-                print(load_dict[0]['paragraphs'])
-                res = re.findall(r'[\u4E00-\u9FA5]+', load_dict[0]['paragraphs'][0])
-                print(res)
-                print(load_dict[0]['title'])
-                print(load_dict[0]['id'])
+        self.get_sentence()
+        self.get_disturb(number, len(self.right_answer[number]))
+        random.shuffle(self.questions[number])
+        print(self.questions[number])
 
     def print_grade(self):
+        '''打印成绩'''
         print(self.grade)
 
 
