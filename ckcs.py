2e0535479c0d004963f3092cdd25ee743bc73a41
lxxlccly <975716738@qq.com>
2020-03-26 11:03:51 +0800

    修改了题目生成函数和回答验证

--- ckcs.py
+++ ckcs.py
@@ -7,7 +7,7 @@ import json
 import random
 import time_limit
 
-LIMIT_TIME = 20
+LIMIT_TIME = 60
 
 
 class PoetGame(object):
@@ -18,12 +18,12 @@ class PoetGame(object):
         self.answers = [''] * self.question_amount
         self.grade = 0
         self.questions = []
-        self.poet_number = []
         self.all_poet = []
 
     def start(self):
         '''开始游戏'''
         self.get_question()
+        self.get_poet_library()
         a_threading = time_limit.MyThread(target=self.cycle)
         a_threading.start()
         a_threading.join()
@@ -38,36 +38,46 @@ class PoetGame(object):
                         print('{0:>2}、{1}'.format(i + 1, self.questions[i]))
                     else:
                         print('{0:>2}、{1}'.format(i + 1, self.questions[i]), end=' ')
-            print('请按输入编号选择问题')
+            print('请输入编号选择问题')
             number = input()
             print('请回答：')
             answer = input()
             re_answer = re.findall(r'[\u4E00-\u9FA5]+', answer)
             for i in range(len(self.all_poet)):
-                if self.questions[int(number)] == self.all_poet[i]['chapter']:
-                    for j in range(len(self.all_poet[i]['paragraphs'])):
+                for j in range(len(self.all_poet[i]['paragraphs'])):
+                    if self.all_poet[i]['chapter'].find(self.questions[int(number) - 1]) != -1 \
+                            or self.all_poet[i]['paragraphs'][j].find(self.questions[int(number) - 1]) != -1:
                         sentence = re.findall(r'[\u4E00-\u9FA5]+', self.all_poet[i]['paragraphs'][j])
                         if re_answer == sentence:
-                            self.answers[int(number)] = answer
-                            self.unanswered[int(number)] = 0
+                            self.answers[int(number) - 1] = answer
+                            self.unanswered[int(number) - 1] = 0
+            if self.unanswered[int(number) - 1] == 0:
+                print('回答正确')
+            else:
+                print('回答错误')
             if self.unanswered == [0] * self.question_amount:
                 answer_unfinished = 0
         return 1
 
     def get_question(self):
         '''获得题目'''
+        all_question = []
+        with open('./poet/question.txt', encoding="utf-8") as f:
+            for row in f:
+                word = row.replace('\n', '').replace('\r', '')
+                all_question.append(word)
+        while 1:
+            random_poet = random.randint(0, len(all_question) - 1)
+            if all_question[random_poet] not in self.questions:
+                self.questions.append(all_question[random_poet])
+            if len(self.questions) == self.question_amount:
+                break
+
+    def get_poet_library(self):
+        '''诗词库'''
         address = './poet/tssbs.json'
         with open(address, 'r', encoding='utf-8') as load_f:
             self.all_poet = json.load(load_f)
-        while 1:
-            random_poet = random.randint(0, len(self.all_poet) - 1)
-            if random_poet not in self.poet_number:
-                self.poet_number.append(random_poet)
-            if len(self.poet_number) == self.question_amount:
-                break
-        for i in range(self.question_amount):
-            question = self.all_poet[self.poet_number[i]]['chapter']
-            self.questions.append(question)
 
     def print_grade(self):
         '''打印成绩'''
